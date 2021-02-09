b54b50219bfdd9bbc664e6e154c5e7f976b9d2d3
ShootKing233 <shootking233@outlook.com>
2021-02-09 16:58:38 +0800

    updated index.html and 404.html etc.

--- scripts/randomText.js
+++ scripts/randomText.js
@@ -5,7 +5,15 @@ function setRandomText() {
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
             var data = JSON.parse(xhr.responseText);
-            document.getElementById('randomText').innerText = data.hitokoto;
+            var randomTextDiv = document.getElementById('randomTextDiv');
+            var randomText = document.getElementById('randomText');
+            randomTextDiv.removeChild(randomText);
+            var newRandomText = document.createElement('p');
+            newRandomText.setAttribute('onclick', 'setRandomText();');
+            newRandomText.className = 'fadeEffect';
+            newRandomText.id = 'randomText';
+            newRandomText.innerText = data.hitokoto;
+            randomTextDiv.appendChild(newRandomText);
             document.getElementById('textUrl').innerHTML = '<a href="https://hitokoto.cn/?uuid=' + data.uuid + '">来源：一言</a>';
             console.log('成功获取了地址为 https://hitokoto.cn/?uuid=' + data.uuid + ' 的文本：' + data.hitokoto);
         };
